b14b4e2dcfaf0d282f87e62db8f8b19515bcf644
EvanTheBoy <evanparker945@gmail.com>
2023-07-11 11:16:22 +0800

    更改部分前端代码,使其适配本地后端

---
+++ src/services/evanapi-frontend/interfaceInfoController.ts
@@ -0,0 +1,108 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from '@umijs/max';
+
+/** addInterfaceInfo POST /api/interfaceInfo/add */
+export async function addInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoAddRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponselong>('/api/interfaceInfo/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** deleteInterfaceInfo POST /api/interfaceInfo/delete */
+export async function deleteInterfaceInfoUsingPOST(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseboolean>('/api/interfaceInfo/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** getInterfaceInfoVOById GET /api/interfaceInfo/get/vo */
+export async function getInterfaceInfoVOByIdUsingGET(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getInterfaceInfoVOByIdUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseInterfaceInfoVO>('/api/interfaceInfo/get/vo', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** listInterfaceInfoVOByPage POST /api/interfaceInfo/list/page/vo */
+export async function listInterfaceInfoVOByPageUsingPOST(
+  body: API.InterfaceInfoQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageInterfaceInfoVO>('/api/interfaceInfo/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** listMyInterfaceInfoVOByPage POST /api/interfaceInfo/my/list/page/vo */
+export async function listMyInterfaceInfoVOByPageUsingPOST(
+  body: API.InterfaceInfoQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageInterfaceInfoVO>('/api/interfaceInfo/my/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** searchInterfaceInfoVOByPage POST /api/interfaceInfo/search/page/vo */
+export async function searchInterfaceInfoVOByPageUsingPOST(
+  body: API.InterfaceInfoQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageInterfaceInfoVO>('/api/interfaceInfo/search/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** updateInterfaceInfo POST /api/interfaceInfo/update */
+export async function updateInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoUpdateRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseboolean>('/api/interfaceInfo/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
