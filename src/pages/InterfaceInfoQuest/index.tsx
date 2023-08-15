2893a96fedba755529985430ec6629e42ed890d1
EvanTheBoy <evanparker945@gmail.com>
2023-08-15 12:23:10 +0800

    修改接口信息页面

--- src/pages/InterfaceInfoQuest/index.tsx
+++ src/pages/InterfaceInfoQuest/index.tsx
@@ -1,62 +1,55 @@
 import {PageContainer} from '@ant-design/pro-components';
-import {List, message} from 'antd';
+import {Card, Descriptions, message} from 'antd';
 import React, {useEffect, useState} from 'react';
 import {
-  listInterfaceInfoVOByPageUsingPOST
+    getInterfaceInfoVOByIdUsingGET,
 } from "@/services/evanapi-backend/interfaceInfoController";
+import {useParams} from "@@/exports";
 
 
 const Index: React.FC = () => {
   const [loading, setLoading] = useState(false);
-  const [list, setList] = useState<API.InterfaceInfoVO[]>([]);
-  const [total, setTotal] = useState<number>(0)
-
-  const loadData = async (current = 1, pageSize = 5) => {
+  const [data, setData] = useState<API.InterfaceInfoVO>();
+  const params = useParams();
+  const loadData = async () => {
+    if (!params.id) {
+      message.error('参数不存在');
+      return
+    }
     setLoading(true)
     try {
-      const res = await listInterfaceInfoVOByPageUsingPOST({
-        current, pageSize
+      const res = await getInterfaceInfoVOByIdUsingGET({
+          id: Number(params.id)
       });
-      setList(res?.data?.records ?? [])
-      setTotal(res?.data?.total ?? 0)
+      setData(res.data);
     } catch (error: any) {
       message.error('创建失败:' + error.message);
     }
     setLoading(false)
   }
-
   useEffect(() => {
     loadData()
   }, [])
   return (
-    <PageContainer title={"API开放平台"}>
-      <List
-        className="my-list"
-        loading={loading}
-        itemLayout="horizontal"
-        dataSource={list}
-        renderItem={(item) => (
-          <List.Item
-            actions={[<a key="list-loadmore-edit">查看</a>]}
-          >
-            <List.Item.Meta
-              title={<a href="https://ant.design">{item.name}</a>}
-              description={item.description}
-            />
-          </List.Item>
-        )}
-        pagination={{
-            showTotal(total) {
-              return '总数:' + total;
-            },
-            pageSize: 5,
-            total,
-            onChange(page, pageSize) {
-              loadData(page, pageSize)
-            }
-          }
+    <PageContainer title={"查看接口文档"}>
+      <Card>
+        {
+          data ? (
+            <Descriptions title={data.name} column={1}>
+                <Descriptions.Item label="描述">{data.description}</Descriptions.Item>
+                <Descriptions.Item label="接口状态">{data.status ? '正常' : '关闭'}</Descriptions.Item>
+                <Descriptions.Item label="请求地址">{data.url}</Descriptions.Item>
+                <Descriptions.Item label="请求方法">{data.method}</Descriptions.Item>
+                <Descriptions.Item label="请求头">{data.requestHeader}</Descriptions.Item>
+                <Descriptions.Item label="响应头">{data.responseHeader}</Descriptions.Item>
+                <Descriptions.Item label="创建时间">{data.createTime}</Descriptions.Item>
+                <Descriptions.Item label="更新时间">{data.updateTime}</Descriptions.Item>
+            </Descriptions>
+          ) : (
+            <>接口不存在</>
+          )
         }
-      />
+      </Card>
     </PageContainer>
   );
 };
